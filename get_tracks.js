ed5cd669b99a50d3be5653310e404a42f53d0f60
Musa Haydar <musah@umich.edu>
2022-07-24 18:27:02 -0500

    Fixed quotation marks in CSV format

--- get_tracks.js
+++ get_tracks.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const readline = require('readline')
-const fetch = require('isomorphic-unfetch')
-const { getData, getPreview, getTracks, getDetails } = require('spotify-url-info')(fetch)
+const readline = require('readline');
+const fetch = require('isomorphic-unfetch');
+const { getData, getPreview, getTracks, getDetails } = require('spotify-url-info')(fetch);
 
 if(process.argv.length !== 3) {
     console.log("Usage: ./get_tracks.js <filename>");
     console.log("Filename should be a file containing a list of spotify URLs");
     process.exit(0);
+}
+
+// sanitize a string for CSV format
+function sanitize(str) {
+    // internal quotation marks (") should be replaced with double-quotation marks ("")
+    str = str.replace(/"/g, '""');
+    return str
 }
 
 async function readFile() {
@@ -26,7 +33,7 @@ async function readFile() {
     fout.write("\"Artists\",\"Title\",\"Album\",\"Date\",\"URL\",\"Image\"\n");
 
     // read input file and append tracks to CSV file
-    var num_errs = 0
+    var num_errs = 0;
 
     for await (const line of rl) {
         // use getData to get album info
@@ -61,25 +68,26 @@ async function readFile() {
         }
 
         // append results to file
-        fout.write("\"" + trackPreview["artist"] + "\",")
-        fout.write("\"" + trackPreview["title"] + "\",")
-        fout.write("\"" + trackData["album"]["name"] + "\",")
+        fout.write("\"" + sanitize(trackPreview["artist"]) + "\",");
+        fout.write("\"" + sanitize(trackPreview["title"]) + "\",");
+        fout.write("\"" + sanitize(trackData["album"]["name"]) + "\",");
         
         if(trackPreview["date"] === undefined) {
-            fout.write("\"\",")
+            fout.write("\"\",");
         } else {
-            fout.write("\"" + trackPreview["date"] + "\",")
+            fout.write("\"" + trackPreview["date"] + "\",");
         }
 
-        fout.write("\"" + trackPreview["link"] + "\",")
-        fout.write("\"" + trackPreview["image"] + "\"")
-        fout.write("\n")
+        // no need to sanitize URLs
+        fout.write("\"" + trackPreview["link"] + "\",");
+        fout.write("\"" + trackPreview["image"] + "\"");
+        fout.write("\n");
 
-        console.log("Got info for track " + trackPreview["title"])
+        console.log("Got info for track " + trackPreview["title"]);
     }
     
     console.log("\nDONE");
-    console.log("Got errors on " + num_errs + " tracks\n")
+    console.log("Got errors on " + num_errs + " tracks\n");
 }
 
 readFile();
